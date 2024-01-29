c0c21ad6f620544d68f1b8c52262102d75ed6a69
Erik <epstorrs@gmail.com>
2024-01-29 21:03:11 +0000

    ability to save gifs

--- mushroom/visualization/utils.py
+++ mushroom/visualization/utils.py
@@ -1,3 +1,5 @@
+import contextlib
+
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import numpy as np
@@ -6,7 +8,8 @@ import seaborn as sns
 import torch
 import torchvision.transforms.functional as TF
 import tifffile
-from einops import rearrange
+from einops import rearrange, repeat
+from PIL import Image
 
 import mushroom.data.multiplex as multiplex
 import mushroom.data.visium as visium
@@ -120,7 +123,7 @@ def display_labeled_as_rgb(labeled, cmap=None, preserve_indices=False):
         labeled = labeled.numpy()
     
     if preserve_indices:
-        cmap = get_cmap(labeled.max()) if cmap is None else cmap
+        cmap = get_cmap(labeled.max() + 1) if cmap is None else cmap
     else:
         cmap = get_cmap(len(np.unique(labeled))) if cmap is None else cmap
 
@@ -165,7 +168,6 @@ def display_clusters(clusters, cmap=None, figsize=None, horizontal=True, preserv
         return axs
 
 
-
 def display_legend(labels, cmap, ax=None):
     if ax is None:
         fig, ax = plt.subplots()
@@ -182,4 +184,42 @@ def show_groups(clusters, groups):
     mapping.update({i:-1 for i in np.unique(clusters) if i not in groups})
     neigh_ids = np.vectorize(mapping.get)(clusters)
     display_clusters(neigh_ids)
+
+
+def volume_to_gif(volume, is_probs, filepath, axis=0, duration=200):
+    if is_probs:
+        imgs = [repeat(img, 'h w -> h w c', c=3)
+                    for img in volume.swapaxes(0, axis)]
+    else:
+        imgs = [display_labeled_as_rgb(labeled, preserve_indices=True)
+                for labeled in volume.swapaxes(0, axis)]
+    imgs = [((img / img.max()) * 255.).astype(np.uint8) for img in imgs]
+
+    ims = [Image.fromarray(x, mode="RGB")
+                for x in imgs]
+    ims[0].save(fp=filepath, format='GIF', append_images=ims,
+             save_all=True, duration=duration, loop=0)
     
+def save_reference_gif(rgb, filepath, axis=0, duration=200, color=(1., 0., 0.), thickness=1):
+    if not isinstance(color, np.ndarray):
+        color = np.asarray(color)
+    if rgb.max() <= 1.:
+        rgb /= rgb.max()
+        rgb *= 255.
+        rgb = rgb.astype(np.uint8)
+        color = (color * 255.).astype(np.uint8)
+    
+    n_frames = rgb.shape[axis]
+    imgs = []
+    for frame in range(n_frames):
+        selections = tuple([slice(None) if i!=axis else slice(i, i+thickness) for i in range(n_frames)])
+        selections = tuple([slice(None) if i!=axis else slice(frame, frame + thickness) for i in range(len(rgb.shape))])
+        new = rgb.copy()
+        new[selections] = color
+        imgs.append(new)
+        
+    ims = [Image.fromarray(x, mode="RGB")
+                for x in imgs]
+    ims[0].save(fp=filepath, format='GIF', append_images=ims,
+             save_all=True, duration=duration, loop=0)
+      
